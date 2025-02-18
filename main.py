298028c176c68372afd471526e39abd99787153c
mmiskatul <masabimiskat@gmail.com>
2025-02-18 08:52:12 +0600

    added music for alram

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import time
 import datetime
 import pygame
 
-def set_alram(alram):
-    pass
+def set_alram(alarm_time):
+    print(f"Alram set for {alarm_time}")
 if __name__=="__main__":
     alarm_time=input("Enter the alarm time (HH:MM:SS) : ")
     set_alram(alarm_time)
