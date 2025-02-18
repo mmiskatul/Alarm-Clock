ac69ed170c6e74543896827986da83e5f4e1177b
mmiskatul <masabimiskat@gmail.com>
2025-02-18 09:12:09 +0600

    alarm done successfully

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ import time
 import datetime
 import pygame
 
-def set_alram(alarm_time):
-    print(f"Alram set for {alarm_time}")
-if __name__=="__main__":
-    alarm_time=input("Enter the alarm time (HH:MM:SS) : ")
-    set_alram(alarm_time)
+def set_alarm(alarm_time):
+    print(f"Alarm set for {alarm_time}")
+    sound_file = "music.mp3"
+    is_running = True
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(sound_file)
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("WAKE UP! Motherfucker")
+
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+
+            is_running = False 
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
